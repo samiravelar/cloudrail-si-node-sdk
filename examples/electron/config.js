793f172ee981fed4fde8343afe3eed5e541a314a
Patrick Stoklasa <patrick@cloudrail.com>
2017-05-25 16:09:43 +0200

    Enhance Electron sample

---
+++ examples/electron/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    licenseKey: "[Your License Key]",
+    dbCredentials: {
+        clientId: "[Client ID]",
+        clientSecret: "[Client Secret]",
+        redirectUri: "http://localhost:12345/auth"
+    },
+    boxCredentials: {
+        clientId: "[Client ID]",
+        clientSecret: "[Client Secret]",
+        redirectUri: "http://localhost:12345/auth"
+    },
+    gdCredentials: {
+        clientId: "[Client ID]",
+        clientSecret: "[Client Secret]",
+        redirectUri: "http://localhost:12345/auth"
+    },
+    odCredentials: {
+        clientId: "[Client ID]",
+        clientSecret: "[Client Secret]",
+        redirectUri: "http://localhost:12345/auth"
+    }
+};
--- examples/electron/renderer.js
+++ examples/electron/renderer.js
@@ -1,43 +1,184 @@
 const cloudrail = require("cloudrail-si");
-cloudrail.Settings.setKey("[Your CloudRail license key]");
+const fs = require("fs");
+const {licenseKey, dbCredentials, gdCredentials, boxCredentials, odCredentials} = require("./config.js");
 const {BrowserWindow} = require("electron").remote;
+const {dialog} = require('electron').remote;
 
-const logoutButton = document.getElementById("logout-button");
-const spaceButton = document.getElementById("space-button");
-const spacePane = document.getElementById("space-pane");
+cloudrail.Settings.setKey(licenseKey);
 
-const DB_CREDS = "dropboxCredentials";
+const STATE = "state";
+const DB_AUTH = "dbauth";
+const BOX_AUTH = "boxauth";
+const GD_AUTH = "gdauth";
+const OD_AUTH = "odauth";
 
-const clientId = "[clientID]";
-const clientSecret = "[clientSecret]";
-const redirectUrl = "http://localhost:12345/auth"; // can be any redirect URL registered in your Dropbox app
-const state = "state";
+const fileList = document.getElementById("file-list");
+const uploadBtn = document.getElementById("upload");
+const dbBtn = document.getElementById("dropbox-btn");
+const boxBtn = document.getElementById("box-btn");
+const gdBtn = document.getElementById("googledrive-btn");
+const odBtn = document.getElementById("onedrive-btn");
+
+let currentService;
+
+
+// ---------------------------------------------------------------------------------------------------
+// 									INSTANTIATE SERVICES
+// ---------------------------------------------------------------------------------------------------
+
 
 const dropbox = new cloudrail.services.Dropbox(
-	cloudrail.RedirectReceivers.getElectronAuthenticator(BrowserWindow, redirectUrl), 
-	clientId, 
-	clientSecret, 
-	redirectUrl, 
-	state
+	cloudrail.RedirectReceivers.getElectronAuthenticator(BrowserWindow, dbCredentials.redirectUri), 
+	dbCredentials.clientId, 
+	dbCredentials.clientSecret, 
+	dbCredentials.redirectUri, 
+	STATE
 );
-
 // Restore credentials if present
-let credentials = window.localStorage.getItem(DB_CREDS);
-if (credentials) dropbox.loadAsString(credentials);
+if (window.localStorage.getItem(DB_AUTH)) dropbox.loadAsString(window.localStorage.getItem(DB_AUTH));
 
-spaceButton.addEventListener("click", event => {
-	spacePane.textContent = "Please wait";
-	dropbox.getAllocation((err, allocation) => {
-		if (err) {
-			spacePane.textContent = "Could not get space allocation";
-		} else {
-			window.localStorage.setItem(DB_CREDS, dropbox.saveAsString()); // save/update credentials persistently to avoid unnecessary logins
-			spacePane.textContent = "Space used in Bytes: " + allocation.used + " of " + allocation.total;
+const box = new cloudrail.services.Box(
+	cloudrail.RedirectReceivers.getElectronAuthenticator(BrowserWindow, boxCredentials.redirectUri), 
+	boxCredentials.clientId, 
+	boxCredentials.clientSecret, 
+	boxCredentials.redirectUri, 
+	STATE
+);
+// Restore credentials if present
+if (window.localStorage.getItem(BOX_AUTH)) box.loadAsString(window.localStorage.getItem(BOX_AUTH));
+
+const googledrive = new cloudrail.services.GoogleDrive(
+	cloudrail.RedirectReceivers.getElectronAuthenticator(BrowserWindow, gdCredentials.redirectUri), 
+	gdCredentials.clientId, 
+	gdCredentials.clientSecret, 
+	gdCredentials.redirectUri, 
+	STATE
+);
+// Restore credentials if present
+if (window.localStorage.getItem(GD_AUTH)) googledrive.loadAsString(window.localStorage.getItem(GD_AUTH));
+
+const onedrive = new cloudrail.services.OneDrive(
+	cloudrail.RedirectReceivers.getElectronAuthenticator(BrowserWindow, odCredentials.redirectUri), 
+	odCredentials.clientId, 
+	odCredentials.clientSecret, 
+	odCredentials.redirectUri, 
+	STATE
+);
+// Restore credentials if present
+if (window.localStorage.getItem(OD_AUTH)) onedrive.loadAsString(window.localStorage.getItem(OD_AUTH));
+
+
+// ---------------------------------------------------------------------------------------------------
+// 											ON CLICK LISTENERS
+// ---------------------------------------------------------------------------------------------------
+
+
+dbBtn.addEventListener("click", event => {
+	triggerService(dropbox, DB_AUTH);
+});
+
+boxBtn.addEventListener("click", event => {
+	triggerService(box, BOX_AUTH);
+});
+
+gdBtn.addEventListener("click", event => {
+	triggerService(googledrive, GD_AUTH);
+});
+
+odBtn.addEventListener("click", event => {
+	triggerService(onedrive, OD_AUTH);
+});
+
+uploadBtn.addEventListener("click", event => {
+	// Open a dialog to allow the user to select a file
+	dialog.showOpenDialog((fileNames) => {
+		if(fileNames === undefined) {
+			return;
 		}
+
+		let path = fileNames[0];
+		let pathComponents = path.split("/");
+		let fileName = pathComponents[pathComponents.length - 1];
+		let stats = fs.statSync(path);
+		let fileSize = stats.size;
+		let stream = fs.createReadStream(path);
+
+		currentService.upload("/" + fileName, stream, fileSize, false, (err) => {
+			if(err) {
+				dialog.showErrorBox("Error", "Upload failed, please try again!");
+				return;
+			}
+
+			addFile({
+				name: fileName,
+				folder: false
+			});
+		});
 	});
 });
 
-logoutButton.addEventListener("click", event => {
-	dropbox.logout(); // make the dropbox object forget about the user
-	window.localStorage.removeItem(DB_CREDS); // remove the credentials from localstorage
-});
+
+// ---------------------------------------------------------------------------------------------------
+// 										HELPER METHODS
+// ---------------------------------------------------------------------------------------------------
+
+
+function triggerService(service, tag) {
+	service.getChildren("/", (err, files) => {
+		if (err) {
+			console.log(JSON.stringify(err));
+		} else {
+			window.localStorage.setItem(tag, service.saveAsString()); // save/update credentials persistently to avoid unnecessary logins
+			displayFiles(files);
+			currentService = service;
+		}
+	});
+}
+
+function displayFiles(files) {
+	while(fileList.firstChild) {
+		fileList.removeChild(fileList.firstChild);
+	}
+
+	files.forEach(addFile);
+}
+
+function addFile(file) {
+	let li = document.createElement("li");
+	li.textContent = file.name;
+	fileList.appendChild(li);
+
+	if(!file.folder) {
+		li.setAttribute("style", "cursor: pointer; color: blue");
+		li.addEventListener("click", saveFile(file.name));
+	}
+}
+
+function saveFile(fileName) {
+	return function() {
+		dialog.showSaveDialog({
+			defaultPath: fileName
+		}, (path) => {
+			if(path === undefined) {
+				return;
+			}
+
+			currentService.download("/" + fileName, (err, stream) => {
+				if(err) {
+					dialog.showErrorBox("Error", "Download failed, please try again!");
+					return;
+				}
+
+				stream.on("end", () => {
+					dialog.showMessageBox({
+						type: "info",
+						title: "Success",
+						message: "File saved successfully!"
+					});
+				});
+				let writeStream = fs.createWriteStream(path);
+				stream.pipe(writeStream);
+			});
+		});
+	};
+}
